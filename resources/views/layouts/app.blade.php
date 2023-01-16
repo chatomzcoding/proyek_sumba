32b620e697c698e507323cc90936a9748da39464
chatomzcoding <coding.chatomz@gmail.com>
2023-01-16 13:29:40 +0700

    persiapan awal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,14 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="{{ url('public/css/app.css')}}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 
         <!-- Styles -->
         @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
@@ -40,5 +42,7 @@
         @stack('modals')
 
         @livewireScripts
+        <script src="{{ url('public/js/app.js')}}""></script>
+
     </body>
 </html>
